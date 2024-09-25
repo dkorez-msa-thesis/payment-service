f3795007d08a25297c41dd9c7708bf09d0617e8c
dkorez <dejan.korez@gmail.com>
2024-09-25 19:48:52 +0200

    fixed order processing

--- src/main/java/dev/dkorez/msathesis/catalog/entity/PaymentDao.java
+++ src/main/java/dev/dkorez/msathesis/catalog/entity/PaymentDao.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 @Data
 public class PaymentDao {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
 
     @Column(name = "checkout_id")
